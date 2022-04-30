8e16f518be987abf2d727dcdb57ccadfa4cd8450
Kirill Naydovich <kirillnaydovich@gmail.com>
2022-04-30 21:39:40 +0300

    complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+    let baseStr = `${str}`;
+    const separator = "+";
+    const additionSeparator = "|"
+
+    if (!options || !Object.keys(options)) {
+        return baseStr;
+    }
+
+    if (options.addition !== undefined) {
+        options.addition = `${options.addition}`;
+
+        if (options.additionRepeatTimes) {
+            if (options.additionSeparator) {
+                baseStr += concat(options.addition, options.additionRepeatTimes, options.additionSeparator);
+            } else {
+                baseStr += concat(options.addition, options.additionRepeatTimes, additionSeparator);
+            }
+        } else {
+            baseStr += options.addition;
+        }
+    }
+
+    if (options.repeatTimes) {
+        if (options.separator) {
+            return concat(baseStr, options.repeatTimes, options.separator);
+        } else {
+            return concat(baseStr, options.repeatTimes, separator);
+        }
+    }
+
+    return baseStr;
+
+    function concat(str, repeat, separator) {
+        const group = [];
+        for (let i = 0; i < repeat; i++) {
+            group.push(str);
+        }
+        return group.join(`${separator}`);
+    }
 }
 
+
 module.exports = {
-  repeater
+    repeater
 };
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,11 +16,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+    let result = 0;
+    const columnLimit = [];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (columnLimit.some((el) => el === j) || matrix[i][j] < 0) {
+                continue;
+            }
+            if (matrix[i][j] === 0) {
+                columnLimit.push(j);
+            } else {
+                result += matrix[i][j];
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = {
-  getMatrixElementsSum
+    getMatrixElementsSum
 };
